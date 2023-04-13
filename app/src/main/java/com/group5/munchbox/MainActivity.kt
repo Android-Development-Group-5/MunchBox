f53ff95dc565e3c5ad484c7632032a3882c5bf97
Allen Schmerler <rockdhor@gmail.com>
2023-04-12 22:47:33 -0400

    Implemented login/register process

--- app/src/main/java/com/group5/munchbox/MainActivity.kt
+++ app/src/main/java/com/group5/munchbox/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.group5.munchbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        }
+        findViewById<TextView>(R.id.hello).setOnClickListener{
+            Firebase.auth.signOut()
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        }
     }
 }

ce15092655cb11ee349651d3632352e49b278d5c
Aanika-Sadana <As@github1>
2023-04-21 15:01:04 -0400

    visual improvements in mobile view

--- app/src/main/java/com/group5/munchbox/MyFeedFragment.kt
+++ app/src/main/java/com/group5/munchbox/MyFeedFragment.kt
@@ -5,6 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.widget.ContentLoadingProgressBar
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import okhttp3.Headers
+import org.json.JSONException
 
 /**
  * A simple [Fragment] subclass.
@@ -12,6 +31,7 @@ import androidx.fragment.app.Fragment
  * create an instance of this fragment.
  */
 class MyFeedFragment : Fragment() {
+    private val userRecipes = mutableListOf<RecipeItem>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }

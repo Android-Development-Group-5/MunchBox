447fe8ae2e2d7ee8e6b73323c219a28be57a9a3d
Aanika-Sadana <As@github1>
2023-05-01 21:04:02 -0400

    upvotes/downvotes also reflected in detailed view

--- app/src/main/java/com/group5/munchbox/MyFeedDetailedActivity.kt
+++ app/src/main/java/com/group5/munchbox/MyFeedDetailedActivity.kt
@@ -1,5 +1,6 @@
 package com.group5.munchbox
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.gson.Gson
 
@@ -21,9 +25,15 @@ class MyFeedDetailedActivity: AppCompatActivity() {
         setContentView(R.layout.activity_detailed)
 
         val gson = Gson()
-        val recipe = gson.fromJson(intent.getStringExtra("my_recipe_extra"), RecipeModel::class.java)
+        val recipe = gson.fromJson(intent.getStringExtra("my_feed_recipe_extra"), RecipeModel::class.java)
         val recipeIngredientsList = recipe.recipeIngredients
         var storageReference = FirebaseStorage.getInstance().getReference()
+
+        val user = FirebaseAuth.getInstance().currentUser
+        var userId: String = ""
+        if(user != null){
+            userId = user.uid
+        }
 
         val recipeImageView: ImageView = findViewById(R.id.detailed_recipe_image)
         val recipeName: TextView = findViewById(R.id.detailed_recipe_name)
@@ -32,6 +42,11 @@ class MyFeedDetailedActivity: AppCompatActivity() {
         val detailedActivityRecyclerView: RecyclerView = findViewById(R.id.recyclerView)
         val recipeInstruction: TextView = findViewById(R.id.detailed_recipe_instructions)
         val postInteraction = findViewById<View>(R.id.postInteraction)
+        var upvoteCount: TextView = findViewById(R.id.upvoteCount)
+        var downvoteCount: TextView = findViewById(R.id.downvoteCount)
+        var commentCount: TextView = findViewById(R.id.commentCount)
+        var upvote: ImageView = findViewById(R.id.upvote)
+        var downvote: ImageView = findViewById(R.id.downvote)
 
         detailedActivityRecyclerView.layoutManager = LinearLayoutManager(this)
 
@@ -47,6 +62,118 @@ class MyFeedDetailedActivity: AppCompatActivity() {
             .load(storageReference.child("$imgPath.png"))
             .transform(CenterCrop())
             .into(recipeImageView)
+
+
+        commentCount.text = "0"
+        if(recipe.comments != null){
+            commentCount.text = recipe.comments?.size.toString()
+        }
+        upvoteCount.text = "0"
+        if(recipe.upvotes != null){
+            upvoteCount.text = recipe.upvotes?.size.toString()
+        }
+        downvoteCount.text = "0"
+        if(recipe.downvotes != null){
+            downvoteCount.text = recipe.downvotes?.size.toString()
+        }
+        upvote.setColorFilter(Color.rgb(0, 0, 0))
+        if(recipe.upvotes != null){
+            if(recipe.upvotes?.contains(userId) == true){
+                upvote.setColorFilter(Color.rgb(0, 255, 0))
+            }
+        }
+        downvote.setColorFilter(Color.rgb(0, 0, 0))
+        if(recipe.downvotes != null){
+            if(recipe.downvotes?.contains(userId) == true){
+                downvote.setColorFilter(Color.rgb(0, 0, 255))
+            }
+        }
+        upvote.setOnClickListener {
+            val database = Firebase.database
+            val myRef = database.getReference("Recipes")
+
+            var recipeId = ""
+            if (recipe.recipeId != null) {
+                recipeId = recipe.recipeId.toString()
+            }
+            val ruserID = recipe.userId
+            val recipeName = recipe.recipeName
+            val recipeDetails = recipe.recipeDetails
+            val recipeImage = recipe.recipeImage
+            val recipeInstructions = recipe.recipeInstructions
+            val recipeIngredients = recipe.recipeIngredients
+            val userEmail = recipe.userEmail
+            var upvoters1: MutableList<String>? = mutableListOf<String>()
+            if (recipe.upvotes != null){
+                upvoters1 = recipe.upvotes?.toMutableList()
+            }
+            if(upvoters1?.contains(user?.uid) == false){
+                upvoters1.add(userId)
+            }
+            val upvotes = upvoters1
+
+            var downvoters1: MutableList<String>? = mutableListOf<String>()
+            if (recipe.downvotes != null){
+                downvoters1 = recipe.downvotes?.toMutableList()
+            }
+            if(downvoters1?.contains(user?.uid) == true){
+                downvoters1.remove(userId)
+            }
+            val downvotes = downvoters1
+            val comments = recipe.comments
+            val recipe = RecipeModel(recipeId, ruserID, recipeName, recipeDetails, recipeImage, recipeInstructions, recipeIngredients, userEmail, upvotes, downvotes, comments)
+            myRef.child(recipeId).setValue(recipe)
+                .addOnSuccessListener{
+                    upvote.setColorFilter(Color.rgb(0, 255, 0))
+                    downvote.setColorFilter(Color.rgb(0, 0, 0))
+                    upvoteCount.text = upvotes?.size.toString()
+                    downvoteCount.text = downvotes?.size.toString()
+                }
+
+        }
+        downvote.setOnClickListener {
+            val database = Firebase.database
+            val myRef = database.getReference("Recipes")
+
+            var recipeId = ""
+            if (recipe.recipeId != null) {
+                recipeId = recipe.recipeId.toString()
+            }
+            val ruserID = recipe.userId
+            val recipeName = recipe.recipeName
+            val recipeDetails = recipe.recipeDetails
+            val recipeImage = recipe.recipeImage
+            val recipeInstructions = recipe.recipeInstructions
+            val recipeIngredients = recipe.recipeIngredients
+            val userEmail = recipe.userEmail
+            var upvoters2: MutableList<String>? = mutableListOf<String>()
+            if (recipe.upvotes != null){
+                upvoters2 = recipe.upvotes?.toMutableList()
+            }
+            if(upvoters2?.contains(user?.uid) == true){
+                upvoters2.remove(userId)
+            }
+            val upvotes = upvoters2
+
+            var downvoters2: MutableList<String>? = mutableListOf<String>()
+            if (recipe.downvotes != null){
+                downvoters2 = recipe.downvotes?.toMutableList()
+            }
+            if(downvoters2?.contains(user?.uid) == false){
+                downvoters2.add(userId)
+            }
+            val downvotes = downvoters2
+            val comments = recipe.comments
+            val recipe = RecipeModel(recipeId, ruserID, recipeName, recipeDetails, recipeImage, recipeInstructions, recipeIngredients, userEmail, upvotes, downvotes, comments)
+            myRef.child(recipeId).setValue(recipe)
+                .addOnSuccessListener{
+                    downvote.setColorFilter(Color.rgb(0, 0, 255))
+                    upvote.setColorFilter(Color.rgb(0, 0, 0))
+                    upvoteCount.text = upvotes?.size.toString()
+                    downvoteCount.text = downvotes?.size.toString()
+                }
+        }
+
     }
 
     private inner class RecyclerViewAdapter(private val ingredients: MutableList<IngredientsItem>?): RecyclerView.Adapter<RecyclerViewAdapter.IngredientViewHolder>() {
